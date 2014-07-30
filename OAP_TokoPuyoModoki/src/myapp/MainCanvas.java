8688b0ecfaabd3dc288809dc6c092d9528eaf26c
Leonardone <neetsdkasu@yahoo.co.uk>
2014-07-30 19:53:56 +0900

    表示テスト

--- OAP_TokoPuyoModoki/src/myapp/MainCanvas.java
+++ OAP_TokoPuyoModoki/src/myapp/MainCanvas.java
@@ -3,20 +3,45 @@
  */
 package myapp;
 
+import java.io.IOException;
+
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.GameCanvas;
+import javax.microedition.lcdui.game.TiledLayer;
 
 /**
  * @author Leonardone
  *
  */
-class MainCanvas extends GameCanvas {
+class MainCanvas extends GameCanvas implements Runnable {
 
+	private Graphics g = null;
+	private Image puyoImage = null;
+	private TiledLayer field = null;
 	/**
-	 * @param suppressKeyEvents
+	 * @throws IOException 
+	 * 
 	 */
-	public MainCanvas(boolean suppressKeyEvents) {
-		super(suppressKeyEvents);
-		// TODO 自動生成されたコンストラクター・スタブ
+	public MainCanvas() throws IOException {
+		super(false);
+		
+		puyoImage = Image.createImage("/puyo.png");
+		
+		field = new TiledLayer(10, 15, puyoImage, 24, 20);
+		field.setPosition(0, -15);
+		field.fillCells(2, 1, 6, 12, 2); // MainField
+		field.fillCells(0, 0, 2, 15, 8); // Left Wall
+		field.fillCells(8, 0, 2, 15, 8); // Right Wall
+		field.fillCells(2, 0, 6, 1, 8);  // Upper Wall
+		field.fillCells(2, 13, 6, 2, 8); // Under Wall
+
+		g = getGraphics();
+	}
+	public void run() {
+		// TODO 自動生成されたメソッド・スタブ
+		field.paint(g);
+		flushGraphics();
 	}
 
 }
--- OAP_TokoPuyoModoki/src/myapp/TokoPuyoModokiMIDlet.java
+++ OAP_TokoPuyoModoki/src/myapp/TokoPuyoModokiMIDlet.java
@@ -3,6 +3,14 @@
  */
 package myapp;
 
+import java.io.IOException;
+
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
@@ -10,12 +18,32 @@ import javax.microedition.midlet.MIDletStateChangeException;
  * @author Leonardone
  *
  */
-public class TokoPuyoModokiMIDlet extends MIDlet {
+public class TokoPuyoModokiMIDlet extends MIDlet implements CommandListener {
 
+	private Command exitCommand = null; 
+	private MainCanvas canvas = null;
+	private Thread mainThread = null;
+	
 	/**
 	 * 
+	 * 
 	 */
-	public TokoPuyoModokiMIDlet() {
+	public TokoPuyoModokiMIDlet()  {
+		Displayable disp = null;
+		exitCommand = new Command("EXIT", Command.EXIT, 1);
+		try {
+			canvas = new MainCanvas();
+			disp = canvas;
+			disp.addCommand(exitCommand);
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+			canvas = null;
+			Alert alert = new Alert("ERROR", "起動に失敗しました", null, AlertType.ERROR);
+			disp = alert;
+		}
+		disp.setCommandListener(this);
+		Display.getDisplay(this).setCurrent(disp);
 	}
 
 	/* (非 Javadoc)
@@ -23,7 +51,6 @@ public class TokoPuyoModokiMIDlet extends MIDlet {
 	 */
 	protected void destroyApp(boolean unconditional)
 			throws MIDletStateChangeException {
-		// TODO 自動生成されたメソッド・スタブ
 
 	}
 
@@ -31,7 +58,6 @@ public class TokoPuyoModokiMIDlet extends MIDlet {
 	 * @see javax.microedition.midlet.MIDlet#pauseApp()
 	 */
 	protected void pauseApp() {
-		// TODO 自動生成されたメソッド・スタブ
 
 	}
 
@@ -39,8 +65,16 @@ public class TokoPuyoModokiMIDlet extends MIDlet {
 	 * @see javax.microedition.midlet.MIDlet#startApp()
 	 */
 	protected void startApp() throws MIDletStateChangeException {
-		// TODO 自動生成されたメソッド・スタブ
+		if (mainThread == null && canvas != null) {
+			mainThread = new Thread(canvas);
+			mainThread.start();
+		}
+	}
 
+	public void commandAction(Command c, Displayable d) {
+		if (c.equals(exitCommand) || c.equals(Alert.DISMISS_COMMAND)) {
+			notifyDestroyed();
+		}
 	}
 
 }
