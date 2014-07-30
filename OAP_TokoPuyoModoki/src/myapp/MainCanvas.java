8e23e7691787986dae129d9a1805ea40c7cbc94d
Leonardone <neetsdkasu@yahoo.co.uk>
2014-07-30 19:33:26 +0900

    add MainCanvas.java

---
+++ OAP_TokoPuyoModoki/src/myapp/MainCanvas.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package myapp;
+
+import javax.microedition.lcdui.game.GameCanvas;
+
+/**
+ * @author Leonardone
+ *
+ */
+class MainCanvas extends GameCanvas {
+
+	/**
+	 * @param suppressKeyEvents
+	 */
+	public MainCanvas(boolean suppressKeyEvents) {
+		super(suppressKeyEvents);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
