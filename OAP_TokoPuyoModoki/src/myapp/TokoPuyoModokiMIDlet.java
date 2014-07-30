5631d58328ce808a1035e436551fe85463069593
Leonardone <neetsdkasu@yahoo.co.uk>
2014-07-30 19:32:43 +0900

    add TokoPuyoModokiMIDlet.java

---
+++ OAP_TokoPuyoModoki/src/myapp/TokoPuyoModokiMIDlet.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package myapp;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+/**
+ * @author Leonardone
+ *
+ */
+public class TokoPuyoModokiMIDlet extends MIDlet {
+
+	/**
+	 * 
+	 */
+	public TokoPuyoModokiMIDlet() {
+	}
+
+	/* (非 Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#destroyApp(boolean)
+	 */
+	protected void destroyApp(boolean unconditional)
+			throws MIDletStateChangeException {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	/* (非 Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#pauseApp()
+	 */
+	protected void pauseApp() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	/* (非 Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#startApp()
+	 */
+	protected void startApp() throws MIDletStateChangeException {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
